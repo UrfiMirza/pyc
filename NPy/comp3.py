6aaeb3c0c444459ec81b9562cf160b6417152b5d
Agha Urfi Mirza <urfim7@gmail.com>
2022-05-30 14:40:50 +0400

    Add files via upload

---
+++ NPy/comp3.py
@@ -0,0 +1,9 @@
+a = int(input('Enter num a: '))
+b = int(input('Enter num b: '))
+c = int(input('Enter num c: '))
+if a>b and a>c:
+    print('a is biggest')
+elif b>a and b>c:
+    print('b is biggest')
+else:
+    print('c is biggest')
